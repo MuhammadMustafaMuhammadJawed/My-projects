c7247478c3eface5a0f5956ce2164ef516c0344c
Muhammad Mustafa <muhammadmustafamuhammadjawedkh@gmail.com>
2024-03-04 22:20:59 +0500

    javascript first 2 assignment

---
+++ javascript/assignment chapter 1 alerts/app.js
@@ -0,0 +1,7 @@
+// alert("Error! Please enter valid password.")
+// alert("Welcome To JS land... \n happy coding");
+// alert("Welcome To JS Land...")
+// alert("Happy Coding!")
+// var alert = alert("Hello... I can run JS through my web browser's console")
+// var alert = ("Hello... I can run JS through my web browser's console")
+// console.log (alert)
---
+++ javascript/assignment chapter2/app.js
@@ -0,0 +1,27 @@
+
+// var myname = "Muhammad Mustafa"
+// console.log(myname)
+
+// var message =  "Hello World";
+// alert(message)
+
+// var studentName = "John Doe"
+// alert(studentName)
+// var studentage = "15 years old"
+// alert(studentage)
+// var studentCourse = "Certified Mobile Application Development"
+// alert(studentCourse)
+
+// var pizza = "Pizza \npizz \npiz \npi \np"
+// alert(pizza)
+
+// var message = "my email adress is"
+// var email = "muhammadmustafamuhammadjawedkh@gmail.com"
+// alert(message + " " + email)
+
+// var statement = "I am trying to learn javascript from Book"
+// var book = "A smarter way to learn JavaScript"
+//  alert(statement + " " + book)
+
+// document.write("Yah! I can write HTML content through javascript")
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
