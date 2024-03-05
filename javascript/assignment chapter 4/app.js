c2da8ea8b123eafe76a7ff52d6c01cdc86d51a19
Muhammad Mustafa <muhammadmustafamuhammadjawedkh@gmail.com>
2024-03-05 21:57:32 +0500

    assignment 3 and 4

---
+++ javascript/assignment chapter 4/app.js
@@ -0,0 +1,20 @@
+//1
+// var var1 = 1, var2 = 2, var3 = 3;
+//2
+//legal
+// var userName, $userName, user_Name, userName1
+//illegal
+// var alert, 123, %^&, 12userName,
+//3
+// var sentence1 = 'Variable names can only contain, Numbers,$ and ______. For example $my_1stVariable.' 
+// var sentence2 = "Variables must begin with a letter, $ or _____. For example $name, _name or name."
+// var sentence3 = "Variable names are case sensitive."
+// var sentence4 = " Variable names should not be JS keywords." 
+// document.write(sentence1)
+// document.write(sentence2)
+// document.write(sentence3)
+// document.write(sentence4)
+// console.log(sentence1)
+// console.log(sentence2)
+// console.log(sentence3)
+// console.log(sentence4)
---
+++ javascript/assignment chapter3/app.js
@@ -0,0 +1,15 @@
+//1
+// var age = "I am 15 years old"
+// alert(age)
+//2
+// var visiting = "you have visited this site 10 times"
+//  alert(visiting)
+//3
+// var description = "My birth year is"
+// var birthyear = 2007
+// document.write(description + " " + birthyear)
+//4
+// var customerName = "Muhammad Mustafa ordered"
+// var title = "5 T-Shirt(s)"
+// var store = "On XYZ clothing store"
+// document.write(customerName + " "+ title + " "+ store )
