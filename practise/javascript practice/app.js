3075c2b37450f207c52803bd8cf1236931703d7f
Muhammad Mustafa <muhammadmustafamuhammadjawedkh@gmail.com>
2024-05-30 10:47:08 +0500

    javascript practice datatype

---
+++ practise/javascript practice/app.js
@@ -0,0 +1,47 @@
+var Score = null;
+var Converted = Number(Score);
+// console.log(typeof Converted);
+// console.log( Converted);
+
+//true=>1
+//false=>0
+//"333anm"=>NaN
+
+
+// var LoggedIn = 1 true
+// var LoggedIn = 0 false
+// var LoggedIn = "" false
+//  var LoggedIn = "Mustafa" true
+
+// var Conversion = Boolean(LoggedIn)
+// console.log(Conversion);
+
+
+
+
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2%3);
+// console.log(2**3);
+
+
+//************prefix and postfix************//
+// let counter = 100
+// counter++;
+// ++ counter;
+// console.log(counter);
+
+//datatype ==> permitive and non permitive  
+
+//permitive==>string,number,boolean,undefined,symbol,BigInt,Null
+//reference type(Non Permitive)==>array,objects,functions ==> return object
+
+
+// let id = Symbol("abs");
+// let anotherid = Symbol("abs");
+// console.log(id===anotherid); ==> false 
+
+
