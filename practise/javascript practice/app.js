87b95d8d94dede2ddf4be2ab07d5e3abe91a12e2
mustafajawed <muhammadmustafamuhammadjawedkh@gmail.com>
2024-06-12 17:00:46 +0500

    filters, maps , reduce methods

--- practise/javascript practice/app.js
+++ practise/javascript practice/app.js
@@ -542,3 +542,70 @@ let anotherName = new String("Mustafa")
 // console.log(`${items.languageName} is name for language ${items.languageFile}`);
 
 // } )
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+// const newNum = myNums.filter((num)=>(num > 6))
+// const newNum = myNums.filter((num)=>{
+//     return num > 6})
+
+// const newNums = []
+// myNums.forEach((num)=>{
+//     if(num > 5){
+//         newNums.push(num)
+// }});
+// console.log(newNums);
+
+
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+// ];
+
+// let userBooks = books.filter((bk)=>bk.genre==="History")
+// userBooks=books.filter((bk)=>{return bk.publish >= 1995 && bk.genre==="History"})
+// console.log(userBooks);
+
+
+// const num = [1,2,3,4,5,6,7,8,9,10]
+// // let newNum = num.map((nm)=>  nm+10)
+// // let newNum = num.map((nm)=>  { return nm+10})
+
+// let newNum = num.map((nm)=>nm*10)
+//                 .map((nm)=>nm+5)
+//                 .filter((nm)=>nm>40)
+
+// console.log(newNum);
+
+
+// ********************reduce method ***********
+
+// const myNum = [1,2,3]
+// const newNum = myNum.reduce(function (acc,currval) {
+//     console.log(`acc:${acc} and currval:${currval}`);
+//     return currval+acc
+// },5)
+// console.log(newNum);
+
+const shoppingCart = [
+    {
+        itemName: "PY Course",
+        price: 2999
+    },
+    {
+        itemName: "Js Course",
+        price: 3999
+    },
+    {
+        itemName: "Mobile Dev Course",
+        price: 5999
+    }
+]
+
+const total = shoppingCart.reduce((acc,item)=>acc + item.price,0)
+console.log(total);
